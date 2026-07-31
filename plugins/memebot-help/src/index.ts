ca346d4217dab155490b2ff3576b370ed0b4e5bc
sein. <nonsein@foxmail.com>
2026-08-01 06:59:53 +0800

    merge: integrate issue-6 plugins into main

--- plugins/memebot-help/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Context, Schema } from 'koishi'
-
-export const name = 'memebot-help'
-export interface Config {}
-export const Config: Schema<Config> = Schema.object({})
-export function apply(_ctx: Context, _config: Config) {}
